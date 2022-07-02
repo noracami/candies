bfd358898ab1e4161d3a3734dfc6517bef5bf995
noracami <kerke.2011@gmail.com>
2022-07-02 14:00:53 +0800

    by sort()

--- CANDY-001.js
+++ CANDY-001.js
@@ -7,39 +7,12 @@
 
 function sumOfSmallestValues(arr) {
   /*
-    找出最小值的方法，就是像從一條路的一端(起點)出發，看到石頭撿起來比較，如果比自己的石頭小，就把原本的丟掉，保留新的，到終點時，手上就會是最小的。
-    而要找出次小值，就是在手上拿兩顆石頭(A)，撿起新的石頭時(B)，**先跟自己較大的石頭比較**(C)，如果比較小，就交換(D)，到終點時，手上就會有兩顆最小的石頭了。
-    注意上面執行 (C) 時，為了區分最小跟次小，(D) 還會檢查剛才新撿起的石頭是不是最小。
-    因此每一輪做的事情：
-    
-    撿起石頭 : arr[i]
-    
-    比較石頭與手上目前較大的石頭(次小值, bigger)的大小 :
-    
-      如果比較小，就交換 :
-        if(bigger > arr[i]) {
-          bigger = arr[i]
-        }
-
-        接著如果新的這顆石頭比最小的石頭還小，兩者交換 : 
-          if (smaller > arr[i]) {
-            bigger = smaller
-            smaller = arr[i]
-          }
-    迴圈結束，即可得到最小值與次小值，
-    回傳相加結果。
+    陣列由小到大排序後，回傳前兩項相加結果
   */
-  let smaller = bigger = Infinity
-  for (let i = 0; i < arr.length; i++) {
-    if (bigger > arr[i]) {     // 若當前值小於次小值，
-      bigger = arr[i]          // 更新次小值。
-      if (smaller > arr[i]) {  // 又如果當前值小於最小值
-        bigger = smaller       // 更新次小值、
-        smaller = arr[i]       // 更新最小值。
-      }
-    }
-  }  
-  return smaller + bigger
+  arr.sort(function (a, b) {
+    return a - b
+  })
+  return arr[0] + arr[1]
 }
 
 const list1 = [19, 5, 42, 2, 77]
