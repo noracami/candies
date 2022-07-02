ef29b0d8e9d901ed6377e38bd4d126abd8f2d6d3
noracami <kerke.2011@gmail.com>
2022-07-02 09:38:19 +0800

    by for loop

--- CANDY-001.js
+++ CANDY-001.js
@@ -6,11 +6,25 @@
 //   [23, 71, 33, 82, 1] 印出 24
 
 function sumOfSmallestValues(arr) {
-  // 實作程式碼寫在這裡
-  let min = arr.reduce((pre, cur) => pre < cur ? pre : cur)
-  arr.splice(arr.indexOf(min), 1)
-  let biggerThanMin = arr.reduce((pre, cur) => pre < cur ? pre : cur)
-  return min + biggerThanMin
+  let min = arr[0]            // 最小值，並以陣列第0項的值初始化
+  let biggerThanMin = arr[1]  // 次小值，並以陣列第1項的值初始化
+  let tmp = 0                 // 暫存用變數，用於交換前兩項用
+  if (min > biggerThanMin) {  //
+    tmp = min                 // 若最小值大於次小值，將兩者交換
+    min = biggerThanMin       // 因之後迴圈會先比較次小值與當前
+    biggerThanMin = tmp       // 值，故需確定最小值不大於次小值。
+  }
+  for (let i = 2; i < arr.length; i++) { // 從第2項開始逐項比較
+    if (biggerThanMin > arr[i]) {  // 先比較次小值與當前值，若當
+      biggerThanMin = arr[i]       // 前值較小則更新次小值，
+      if (min > biggerThanMin) {   // 並接著比較新的次小值與最小
+        tmp = min                  // 值，若次小值較小則兩者交換。
+        min = biggerThanMin
+        biggerThanMin = tmp
+      }
+    }
+  }                                // 全部元素比較完畢，得到最小
+  return min + biggerThanMin       // 值與次小值，回傳相加結果。
 }
 
 const list1 = [19, 5, 42, 2, 77]
@@ -18,5 +32,3 @@ const list2 = [23, 15, 59, 4, 17]
 
 console.log(sumOfSmallestValues(list1)) // 印出 7
 console.log(sumOfSmallestValues(list2)) // 印出 19
-
-console.log(list1) // [ 19, 5, 42, 77 ] <= 陣列被修改了
