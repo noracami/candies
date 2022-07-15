2330fc3e09e8b78b51044fe1d55a94358e6e5c0f
noracami <kerke.2011@gmail.com>
2022-07-15 20:42:34 +0800

    012, recursive

---
+++ CANDY-012.js
@@ -0,0 +1,24 @@
+// 編號：CANDY-012
+// 程式語言：JavaScript
+// 題目：把數字加總，最終濃縮成個位數
+// 範例：9527 => 9 + 5 + 2 + 7 => 23 => 2 + 3 => 5
+//      1450 => 1 + 4 + 5 + 0 => 10 => 1 + 0 => 1
+
+const numberReducer = (num) => {
+  // 實作寫在這裡
+  /*  f(9527)
+    = f(f(9+5+2+7))
+    = f(f(23))
+    = f(f(f(2+3)))
+    = f(f(f(5)))
+    = f(5)
+   */
+  return num > 9
+    ? numberReducer([...num.toString()].reduce((a, b) => a + +b, 0))
+    : num
+}
+
+console.log(numberReducer(9527)) // 印出 5
+console.log(numberReducer(1450)) // 印出 1
+console.log(numberReducer(5566108)) // 印出 4
+console.log(numberReducer(1234567890)) // 印出 9
