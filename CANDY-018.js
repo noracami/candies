4405f00ee143d2cb0258c9e0acf8c925ecd94bf2
noracami <kerke.2011@gmail.com>
2022-07-23 15:55:38 +0800

    018, refactor

--- CANDY-018.js
+++ CANDY-018.js
@@ -4,6 +4,26 @@
 
 //Math.random() return x , 0 <= x < 1
 function randomNumber(min, max) {
+  const start = min
+  const end = max ?? 0
+  return Math.floor(Math.random() * (end - start)) + start
+
+  // 1
+  return Math.floor(
+    Math.random() * Math.abs(min - (max ?? 0)) + (max ? min : 0)
+  )
+
+  // 2
+  return Math.floor(
+    max ? Math.random() * (max - min) + min : Math.random() * min
+  )
+
+  // 3
+  return max ?? false
+    ? Math.floor(Math.random() * (max - min) + min)
+    : Math.floor(Math.random() * min)
+
+  // 4
   if (max) {
     return Math.floor(Math.random() * (max - min) + min)
   } else {
@@ -11,5 +31,6 @@ function randomNumber(min, max) {
   }
 }
 
-console.log(randomNumber(50)) // 隨機印出 0 ~ 49 之間的任何一個數字
-console.log(randomNumber(5, 30)) // 隨機印出 5 ~ 29 之間的任何一個數字
+console.log([...Array(100)].map((x) => randomNumber(50)))
+// console.log(randomNumber(50)) // 隨機印出 0 ~ 49 之間的任何一個數字
+// console.log(randomNumber(5, 30)) // 隨機印出 5 ~ 29 之間的任何一個數字
