f40d4ba5160b5e4fa3af9724959629b686bcec2f
noracami <kerke.2011@gmail.com>
2022-07-23 17:09:15 +0800

    003.rb, use + instead of .concat

--- candy_003.js.rb
+++ candy_003.js.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # 編號：003
 # 程式語言：Ruby
 # 題目：完成函數的內容，把陣列裡的 0 都移到最後面
@@ -5,7 +7,7 @@
 
 def move_zeros_to_end(arr)
   # 程式碼寫在這裡
-  arr.reject { |x| x.eql? 0 }.concat(arr.select { |x| x.eql? 0 })
+  arr.reject { |x| x.eql? 0 } + arr.select { |x| x.eql? 0 }
 end
 
 list = [false, 1, 0, -1, 2, 0, 1, 3, 'a']
