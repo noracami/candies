0e169bca2bb2b1b186036419a0349a203c08a03e
noracami <kerke.2011@gmail.com>
2022-07-31 12:11:51 +0800

    6132, use Set

--- leetcode/6132. Make Array Zero by Subtracting Equal Amounts.rb
+++ leetcode/6132. Make Array Zero by Subtracting Equal Amounts.rb
@@ -1,26 +1,12 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def minimum_operations(nums)
-  t = nums
-  time = 0
-
-  loop {
-    break if (t.uniq.size == 1 && t.first == 0)
-    time = time + 1
-    # p t
-    # p 'before'
-    s = t.uniq.min(2)
-    s = s.min == 0 ? s.max : s.min
-    t.map!{ |n| n >= s ? n - s : n }
-    # p 'after'
-    # p t
-  }
-  time
+  nums.uniq.select{ |n| n > 0 }.size
 end
 
-# p minimum_operations [1,5,0,3,5]
-# p minimum_operations [0]
-# p minimum_operations [1]
+p minimum_operations [1,5,0,3,5]
+p minimum_operations [0]
+p minimum_operations [1]
 p minimum_operations [2,3,4,5]
 # Input: nums = [1,5,0,3,5]
 # Output: 3
