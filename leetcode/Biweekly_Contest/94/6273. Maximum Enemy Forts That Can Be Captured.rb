8c25f822e90f3acfc8f4a175918e69c544be9d6d
noracami <kerke.2011@gmail.com>
2022-12-24 22:54:17 +0800

    Biweekly 94 Q1 ac

--- leetcode/Biweekly_Contest/94/6273. Maximum Enemy Forts That Can Be Captured.rb
+++ leetcode/Biweekly_Contest/94/6273. Maximum Enemy Forts That Can Be Captured.rb
@@ -4,11 +4,18 @@ def capture_forts(forts)
   forts.chunk_while {|e| e == 0}
     .each_cons(2)
     .map {|a, b| a[-1,1] + b}
+    .select {|e| e.any? 1}
+    .select {|e| e.any? -1}
     .map {|e| e.count 0}
-    .max
+    .max || 0
 end
 
-testcase = [[1,0,0,-1,0,0,0,0,1],[0,0,1,-1]]
+TestCase = [
+  [1,0,0,-1,0,0,0,0,1],
+  [0,0,1,-1],
+  [-1,-1,1,-1,-1,0],
+  [0,-1,-1,0,-1],
+  [-1,-1,0,1,0,0,1,-1,1,0]
+]
 
-p capture_forts(testcase[0])
-p capture_forts(testcase[1])
+TestCase.each {|c| puts capture_forts c}
