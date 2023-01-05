873b8b2838ff4bf8a4bf6fcfa0d6f794a819c4bf
noracami <kerke.2011@gmail.com>
2023-01-05 11:55:02 +0800

    w2 Q2 3. O(N)

--- leetcode/2023/waterball_leetcode_group/w2_sliding_window(two_pointers)/3. Longest Substring Without Repeating Characters.rb
+++ leetcode/2023/waterball_leetcode_group/w2_sliding_window(two_pointers)/3. Longest Substring Without Repeating Characters.rb
@@ -2,17 +2,22 @@
 # @return {Integer}
 def length_of_longest_substring(s)
   substring = {}
-  s = s.chars.zip(0..)
+  left = 0
 
-  s.inject(0) { |memo, each_with_index|
-    letter, idx = each_with_index
-
-    if substring[letter]
-      substring = s[substring[letter] + 1, idx - substring[letter]].to_h
-    else
-      substring[letter] = idx
-    end
-
-    [memo, substring.size].max
+  s.size.times.inject(0) { |memo, idx|
+    left = [left, substring[s[idx]] + 1].max if substring[s[idx]]
+    substring[s[idx]] = idx
+    [memo, idx - left + 1].max
   }
 end
+
+TestCase = [
+  ["abcabcbb", 3],
+  ["bbbbb", 1],
+  ["pwwkew", 3],
+  ["", 0],
+  [" ", 1],
+  ["abcdjfkldsafjbnncvbjkzuijwhuaeoijajkweaae", 9]
+]
+
+TestCase.each { |c, ans| p length_of_longest_substring(c) == ans }
