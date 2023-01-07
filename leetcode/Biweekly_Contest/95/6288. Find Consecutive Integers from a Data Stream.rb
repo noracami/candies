a880d2737d9f3c8a66f6c5033ec71aee8435f667
noracami <kerke.2011@gmail.com>
2023-01-07 23:35:09 +0800

    Biweekly 95 Q2 ac

--- leetcode/Biweekly_Contest/95/6288. Find Consecutive Integers from a Data Stream.rb
+++ leetcode/Biweekly_Contest/95/6288. Find Consecutive Integers from a Data Stream.rb
@@ -1,25 +1,22 @@
 class DataStream
-
-  =begin
-      :type value: Integer
-      :type k: Integer
-  =end
-      def initialize(value, k)
-          
+  attr_accessor :value, :k, :container, :h
+    def initialize(value, k)
+      @value = value
+      @k = k
+      @container = Containers::Deque.new
+      @h = 0
+    end
+    def valid
+      @k == @h
+    end
+    def consec(num)
+      if @container.size >= @k
+        pb = @container.pop_back
+        @h -= 1 if pb == @value
       end
-  
-  
-  =begin
-      :type num: Integer
-      :rtype: Boolean
-  =end
-      def consec(num)
-          
-      end
-  
-  
-  end
-  
-  # Your DataStream object will be instantiated and called as such:
-  # obj = DataStream.new(value, k)
-  # param_1 = obj.consec(num)
+      @container.push_front num
+      @h += 1 if num == @value
+      
+      valid
+    end
+end
