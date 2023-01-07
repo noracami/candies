e7476960c43b2185e68b23f6bd4264d427bbb44b
noracami <kerke.2011@gmail.com>
2023-01-07 20:15:45 +0800

    while -> each

--- leetcode/2023/waterball_leetcode_group/w2_sliding_window(two_pointers)/567. Permutation in String.rb
+++ leetcode/2023/waterball_leetcode_group/w2_sliding_window(two_pointers)/567. Permutation in String.rb
@@ -2,21 +2,20 @@
 # @param {String} s2
 # @return {Boolean}
 def check_inclusion(s1, s2)
-  left, length = 0, s1.size
+  length = s1.size
   freq_s1, freq_s2 = Hash.new(0), Hash.new(0)
   (0...length).each { |idx|
     freq_s1[s1[idx]] += 1
     freq_s2[s2[idx]] += 1
   }
 
-  while left < s2.size - s1.size + 1
+  (0..(s2.size - length)).each { |left|
     return true if freq_s1 == freq_s2
 
     freq_s2[s2[left]] -= 1
     freq_s2.delete(s2[left]) if freq_s2[s2[left]] == 0
     freq_s2[s2[left + length]] += 1
-    left += 1
-  end
+  }
 
   false
 end
