cda7e8abfaee6ecd8676b6191bce16515370273c
noracami <kerke.2011@gmail.com>
2023-01-09 01:39:03 +0800

    Biweekly 95 Q4 wip

--- leetcode/Biweekly_Contest/95/6290. Maximize the Minimum Powered City.rb
+++ leetcode/Biweekly_Contest/95/6290. Maximize the Minimum Powered City.rb
@@ -3,11 +3,21 @@
 # @param {Integer} k
 # @return {Integer}
 def max_power(stations, r, k)
-  
+  stations = stations.map.with_index { |s, i|
+    left = i - r < 0 ? 0 : i - r
+    right = i + r + 1 > stations.size ? stations.size : i + r + 1
+    stations[left, right - left].sum
+  }
+
+  min_station, max_station = stations.minmax
+  return min_station + k if k < max_station - min_station
+  k = k - (max_station - min_station)
+  return max_station + k / stations.size if r == 0
+  min_station + k / (stations.size / r)
 end
 
 TestCase = [
-  [[[1,2,4,5,0], 1, 2], 5],
+  [[ [1,2,4,5,0], 1, 2], 5],
   [[[4,4,4,4], 0, 3], 4],
 ]
 
